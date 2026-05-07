4185800eed95d4a346ecf7f470ce98c4ae1f5be0
sebasgonza1000 <juangonzalezri8@gmail.com>
2026-05-06 21:25:19 -0500

    Agrego método de saludar
    Removed methods for input and summation from Main.java.

--- Main.java
+++ Main.java
@@ -7,37 +7,4 @@ public class Main {
         System.out.println("Hola, bienvenido al programa.");
     }
 
-    // Método para pedir números
-    public static int[] pedirNumeros() {
-
-        Scanner scanner = new Scanner(System.in);
-
-        int a;
-        int b;
-
-        System.out.print("Ingresa el primer número: ");
-        a = scanner.nextInt();
-
-        System.out.print("Ingresa el segundo número: ");
-        b = scanner.nextInt();
-
-        return new int[]{a, b};
-    }
-
-    // Método que suma
-    public static int sumar(int a, int b) {
-        return a + b;
-    }
-
-    // Método principal
-    public static void main(String[] args) {
-
-        saludar();
-
-        int[] numeros = pedirNumeros();
-
-        int resultado = sumar(numeros[0], numeros[1]);
-
-        System.out.println("La suma es: " + resultado);
-    }
-}
+    
