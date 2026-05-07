a8bd925c8f2c9a31e8b812d24597a29633472bb9
sebasgonza1000 <juangonzalezri8@gmail.com>
2026-05-07 10:30:08 -0500

    Integro métodos en main

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
+    // Método que saluda
+    public static void saludar() {
+        System.out.println("Hola, bienvenido al programa.");
+    }
+
     // Método que pide un número
     public static int pedirNumero(String mensaje) {
         Scanner entrada = new Scanner(System.in);
@@ -15,4 +20,15 @@ public class Main {
         return a + b;
     }
 
+    // Método principal que integra todos los métodos
+    public static void main(String[] args) {
+        saludar();
+
+        int numero1 = pedirNumero("Digite el primer número entero positivo: ");
+        int numero2 = pedirNumero("Digite el segundo número entero positivo: ");
+
+        int resultado = sumar(numero1, numero2);
+
+        System.out.println("La suma es: " + resultado);
+    }
 }
